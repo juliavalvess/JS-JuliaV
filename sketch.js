47d3476ef041bd7e28e38f01d3c1d83254446f5d
juliavalvess <julia.pereira.alves@escola.pr.gov.br>
2024-08-14 14:00:30 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(600, 600);//cria um retângulo de x de largura por y de altura
+}
+
+function draw() {
+  background("DarkOrange");//muda cor do fundo
+  fill("green");
+  rect(10,10,80,130);//desenha um retângulo(posição x,posição y,largura,altura)
+  fill("white");
+  rect(90,10,80,130);
+  fill("red");
+  rect(170,10,80,130);
+
+
+
+}
